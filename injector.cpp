cf9ea713ea4d13576b8d228445e01b44999cd1a1
97TeaM <122921936+97TeaM@users.noreply.github.com>
2023-09-30 11:34:03 +0300

    C-Lib-Patcher

--- injector.cpp
+++ injector.cpp
@@ -1,21 +1,16 @@
 /*
-* Copyright The Solar Author.
-* This file is part of the Patch C++ Library.
+* injector.cpp - The Patch C++ Library
+* Copyright (C) 2023 Solar
 *
-* The Patch C++ Library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation; either version 2.1 of the
-* License, or (at your option) any later version.
-*
-* The Libpatch C++ Library is distributed in the hope that it will be useful,
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-*
-* injector.cpp
-* 9/21/2023
-* Updated 9/28/2023
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
 */
 
 #include <dlfcn.h>
